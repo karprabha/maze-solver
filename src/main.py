e471e48ddf4d0e6ad530055665523a9f9024cb85
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2025-05-31 23:49:37 +0530

    update main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+from line import Line
+from point import Point
 from window import Window
 
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
